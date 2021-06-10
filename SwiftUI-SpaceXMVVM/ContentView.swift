aa005176f707c7b22efc8aa6153fec50d330e48d
yusufnacar <y.islamnacar@gmail.com>
2021-06-10 13:36:55 +0300

    Initial Commit

---
+++ SwiftUI-SpaceXMVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-SpaceXMVVM
+//
+//  Created by Yusuf Nacar on 10.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
